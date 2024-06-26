0c2809d8cc62cfb35dfb9592c5ee8c80e78b2eb0
Ibrahim Chikani <147825977+IbrahimDev00@users.noreply.github.com>
2024-06-26 10:35:05 +0530

    Created palindrome.py
    Solution for palindrome problem

---
+++ palindrome/palindrome.py
@@ -0,0 +1,10 @@
+x = int(input())
+
+if x<0 or (x%10 == 0 and x!=0):
+    print(False)
+else:
+    reverse = 0
+    while x>reverse:
+        reverse = reverse*10 + x%10
+        x = x//10
+    print(x==reverse or x==reverse//10)
