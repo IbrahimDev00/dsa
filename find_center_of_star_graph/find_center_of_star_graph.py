1eee04a65f1fb77597c02626e81a19aa48a07269
Ibrahim Chikani <147825977+IbrahimDev00@users.noreply.github.com>
2024-06-27 17:02:18 +0530

    Added question
    Added question

--- find_center_of_star_graph/find_center_of_star_graph.py
+++ find_center_of_star_graph/find_center_of_star_graph.py
@@ -1,3 +1,9 @@
+"""
+Q.)There is an undirected star graph consisting of n nodes labeled from 1 to n. A star graph is a graph where there is one center node and exactly n - 1 edges that connect the center node with every other node.
+You are given a 2D integer array edges where each edges[i] = [ui, vi] indicates that there is an edge between the nodes ui and vi. Return the center of the given star graph.
+"""
+
+
 edges = []
 for _ in range(2):
     edge = list(map(int, input("Enter two integers separated by space: ").split()))
