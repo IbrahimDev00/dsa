9709fa9220fc38d65bc2fb1bbb3939f3b6fc569b
Ibrahim Chikani <147825977+IbrahimDev00@users.noreply.github.com>
2024-06-28 14:19:03 +0530

    Added question
    Added question

--- maximum_total_importance_of_roads/solution.py
+++ maximum_total_importance_of_roads/solution.py
@@ -1,3 +1,11 @@
+"""Q.)You are given an integer n denoting the number of cities in a country. The cities are numbered from 0 to n - 1.
+
+You are also given a 2D integer array roads where roads[i] = [ai, bi] denotes that there exists a bidirectional road connecting cities ai and bi.
+
+You need to assign each city with an integer value from 1 to n, where each value can only be used once. The importance of a road is then defined as the sum of the values of the two cities it connects.
+
+Return the maximum total importance of all roads possible after assigning the values optimally."""
+
 n = int(input())
 roads = list(input())
 arr = [0]*n
