697097d4828d649662f213ccb275401afa4e071b
Ibrahim Chikani <147825977+IbrahimDev00@users.noreply.github.com>
2024-06-29 16:24:38 +0530

    Added question
    Added question

--- all_ancestors_of_a_node_in_dag/solution.py
+++ all_ancestors_of_a_node_in_dag/solution.py
@@ -1,3 +1,11 @@
+"""Q.) You are given a positive integer n representing the number of nodes of a Directed Acyclic Graph (DAG). The nodes are numbered from 0 to n - 1 (inclusive).
+
+You are also given a 2D integer array edges, where edges[i] = [fromi, toi] denotes that there is a unidirectional edge from fromi to toi in the graph.
+
+Return a list answer, where answer[i] is the list of ancestors of the ith node, sorted in ascending order.
+
+A node u is an ancestor of another node v if u can reach v via a set of edges."""
+
 n = int(input())
 edges = input()
 graph = [[] for _ in range(n)]
