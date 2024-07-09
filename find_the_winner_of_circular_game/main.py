0b6b50adcffd8f195bc62fb15d0887bce3ac2082
Ibrahim Chikani <147825977+IbrahimDev00@users.noreply.github.com>
2024-07-09 21:26:53 +0530

    added code

---
+++ find_the_winner_of_circular_game/main.py
@@ -0,0 +1,8 @@
+from collections import deque
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        winner = 0
+        for i in range(2, n+1):
+            winner = (winner + k)%i
+        return winner +1
+        
